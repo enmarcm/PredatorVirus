3f8347c2ea82571fdac0f819edfd91a70c1c5e3a
enmarcm <theenmanuel123@gmail.com>
2024-04-08 23:43:16 -0400

    Add project files.

---
+++ Predator/Service.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Predator
+{
+    partial class Service
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TimerSearch = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.TimerSearch)).BeginInit();
+            // 
+            // TimerSearch
+            // 
+            this.TimerSearch.Enabled = true;
+            this.TimerSearch.Elapsed += new System.Timers.ElapsedEventHandler(this.TimerSearch_Elapsed);
+            // 
+            // Service
+            // 
+            this.ServiceName = "Service";
+            ((System.ComponentModel.ISupportInitialize)(this.TimerSearch)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Timers.Timer TimerSearch;
+    }
+}
---
+++ Predator/Service.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predator
+{
+    partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: Add code here to start your service.
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+
+        private void TimerSearch_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+        }
+    }
+}
